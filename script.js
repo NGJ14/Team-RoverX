9de6d379efc98d08b7f8a4ff416f5ba096f17a3a
naveen george joseph <naveengjoseph@gmail.com>
2024-10-29 12:46:49 +0530

    year-wise team files created

--- script.js
+++ script.js
@@ -7,7 +7,23 @@ document.getElementById("header").innerHTML = `
                 <li><a class="ACTIVEhome" href="./index.html">HOME</a></li>
                 <li><a class="ACTIVErovers" href="./rovers.html">ROVERS</a></li>
                 <li><a class="ACTIVEachievements" href="./achievements.html">ACHIEVEMENTS</a></li>
-                <li><a class="ACTIVEteam" href="./team.html">TEAM</a></li>
+                <div class="dropdown">
+                    <button class="dropbtn">TEAM 
+                        <i class="fa fa-caret-down"></i>
+                    </button>
+                    <div class="dropdown-content">
+                        <a href="./team-2024.html">2024</a>
+                        <a href="./team-2023.html">2023</a>
+                        <a href="./team-2022.html">2022</a>
+                        <a href="./team-2021.html">2021</a>
+                        <a href="./team-2020.html">2020</a>
+                        <a href="./team-2019.html">2019</a>
+                        <a href="./team-2018.html">2018</a>
+                        <a href="./team-2017.html">2017</a>
+                        <a href="./team-2016.html">2016</a>
+                        <a href="./team-2015.html">2015</a>
+                </div>
+  </div> 
                 <li><a class="ACTIVEgallery" href="./gallery.html">GALLERY</a></li>
                 <li><a class="ACTIVEsponsor" href="./sponsors.html">SPONSORS</a></li>
             </div>
@@ -32,7 +48,7 @@ document.getElementById("footer").innerHTML = `
             <p>
                 Creation Labs, <br>
                 Vellore Institute of Technology,<br>
-                Vellore, Tamil Nadu,632014</p>
+                Vellore, Tamil Nadu - 632014</p>
         </div>
         <div>
             <p>
